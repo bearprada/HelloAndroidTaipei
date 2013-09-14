f1568125dc054749c31b1767bb64f12daea79906
Prada Hsiung <bear.prada@gmail.com>
2013-09-14 15:16:25 +0800

    init import the test project

---
+++ hello-android-taipei-test/src/com/cardinalblue/example/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.cardinalblue.example;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.cardinalblue.example.MainActivityTest \
+ * com.cardinalblue.example.tests/android.test.InstrumentationTestRunner
+ */
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public MainActivityTest() {
+        super("com.cardinalblue.example", MainActivity.class);
+    }
+
+}
