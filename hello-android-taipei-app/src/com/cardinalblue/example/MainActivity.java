8c3e3814466986047921c6b00544b444511717ef
Prada Hsiung <bear.prada@gmail.com>
2013-09-26 17:43:16 +0800

    revert to fastSum()

--- hello-android-taipei-app/src/com/cardinalblue/example/MainActivity.java
+++ hello-android-taipei-app/src/com/cardinalblue/example/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends Activity {
                 try {
                     long start = Long.parseLong(mStartText.getText().toString());
                     long end = Long.parseLong(mEndText.getText().toString());
-                    mResultText.setText("Result : " + calculator.multiple((int)start, (int)end));
+                    mResultText.setText("Result : " + calculator.fastSum(start, end));
                 } catch (NumberFormatException ex) {
                     Toast.makeText(MainActivity.this, "please enter the numbers", Toast.LENGTH_SHORT).show();
                 } catch (IllegalArgumentException e) {
